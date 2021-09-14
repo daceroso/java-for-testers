3d1283677619dd131ad2867a956dd18f67c8c754
Daceros <daceros19@gmail.com>
2021-09-14 17:05:41 -0500

    working with classes and objects

---
+++ src/main/java/com/serenitydojo/Cat.java
@@ -0,0 +1,24 @@
+package com.serenitydojo;
+
+public class Cat {
+    public String name;
+    private String favoriteFood;
+    public int age;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+}
--- src/test/java/com/serenitydojo/objects/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/objects/WhenCreatingObjects.java
@@ -1,4 +1,30 @@
 package com.serenitydojo.objects;
 
+import com.serenitydojo.Cat;
+import org.junit.Test;
+
 public class WhenCreatingObjects {
+
+    @Test
+    public void creating_a_cat(){
+        String name = "Felix";
+        String favoriteFood = "Tuna";
+        int  age = 4;
+
+
+        Cat felix = new Cat("Felix");
+        felix.setName("Felix");
+        felix.setFavoriteFood("Tuna");
+
+
+        System.out.println(felix.getFavoriteFood());
+        System.out.println(felix.getName());
+
+        felix.age = 4;
+
+
+        Cat spot = new Cat();
+        spot.name = "Spot";
+        spot.age = 3;
+    }
 }
