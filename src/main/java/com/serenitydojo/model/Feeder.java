9ff6b7e2fcd332635c38a21b992f200ad41aee98
Daceros <daceros19@gmail.com>
2021-09-21 22:25:01 -0500

    Switch

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -5,12 +5,25 @@ public class Feeder {
 
     public String feeds(String animal, boolean isPremium) {
 
-        if (animal.equals("Cat")) {
+        switch (animal) {
+            case "Cat":
+                return isPremium ? "Salmon" : "Tuna";
+            case "Dog":
+                return isPremium ? "Deluxe food" : "Dog food";
+            case "Hampster":
+                return isPremium ? "Lettuce" : "Cabbage";
+            default:
+               return "Don't know this animal - Don't know the food";
+        }
+
+
+
+        /*if (animal.equals("Cat")) {
             return isPremium ? "Salmon" : "Tuna";
         } else if (animal.equals("Dog")) {
             return isPremium ? "Deluxe food" : "Dog food";
         }
-        return isPremium ? "Lettuce" : "Cabbage";
+        return isPremium ? "Lettuce" : "Cabbage";*/
 
     }
 }
