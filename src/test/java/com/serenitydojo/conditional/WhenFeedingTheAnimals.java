d6b03af90b5f49f5c27da5dee865fbafdcbbcc51
Daceros <daceros19@gmail.com>
2021-09-22 09:21:11 -0500

    Collections

--- src/test/java/com/serenitydojo/conditional/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/conditional/WhenFeedingTheAnimals.java
@@ -5,6 +5,11 @@ import com.serenitydojo.model.FoodType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static com.serenitydojo.model.AnimalType.*;
 import static com.serenitydojo.model.FoodType.*;
 
@@ -58,6 +63,52 @@ public class WhenFeedingTheAnimals {
         FoodType food = feeder.feeds(HAMPSTER, true);
 
         Assert.assertEquals(LETTUCE, food);
+    }
+
+    @Test
+    public void listOfColors() {
+
+        String[] color = {"red", "green", "blue"};
+        int[] ages = {1,2,3};
+
+        String[] moreColors = new String[3];
+
+        moreColors[0] = "yellow";
+        moreColors[1] = "purple";
+        moreColors[2] = "pink";
+
+        for (int i = 0; i < moreColors.length; i++) {
+            System.out.println(moreColors[i]);
+        }
+
+        System.out.println();
+
+        List<String> myColors = new ArrayList<>();
+
+        myColors.add("red");
+        myColors.add("blue");
+        myColors.add("purple");
+        myColors.add("red");
+
+        myColors.get(0);
+
+        for (String someColor: myColors) {
+            System.out.println("Color: " + someColor);
+        }
+
+
+        Set<String> myColor = new HashSet<>();
+
+        myColor.add("red");
+        myColor.add("blue");
+        myColor.add("purple");
+        myColor.add("red");
+
+        for (String someColor: myColor) {
+            System.out.println("Color: " + someColor);
+        }
+
+
     }
 
 
